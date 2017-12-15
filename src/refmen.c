bce720512f8ab98fa4eb4e0e986b59096ddd89f3
Max Rylander <max.rylander.2608@student.uu.se>
2017-12-15 17:56:57 +0100

    improvement

--- src/refmen.c
+++ src/refmen.c
@@ -7,20 +7,13 @@
 #define DESTR_SIZE 8
 #define HEADER_SIZE (COUNT_SIZE + DESTR_SIZE)
 
+static size_t CASCADE_LIMIT = 1000;
+
 struct record {
   size_t reference_count;
   function1_t destructor;
 
 } typedef record_t;
-
-struct cascade {
-  size_t limit;
-  //linked list, använd någons listmoduler? eller?
-} typedef cascade_t;
-
-//define global struct for cascade, alloc?? är vi ok med att den ligger på stacken?
-//du måste set_cascade_limit i början nu annars kommer det fucka uuuuur
-static cascade_t cascade;
 
 static record_t *convert_to_record(obj object) {
   record_t *record = object;
@@ -69,14 +62,14 @@ obj allocate(size_t bytes, function1_t destructor) {
   return (obj)record;
 }
 
-void set_cascade_limit(size_t new_limit) {
-  cascade.limit = new_limit;//. eftersom den inte är allokad
+void set_cascade_limit(size_t limit) {
+  CASCADE_LIMIT = limit;
 }
 
 //denna heter read för vet inte om det är samma som get ska göra
 //denna används iallafall i mina tester
 size_t get_cascade_limit(){
-  return cascade.limit;
+  return CASCADE_LIMIT;
 }
 
 obj allocate_array(size_t elements, size_t elem_size, function1_t destructor) {
