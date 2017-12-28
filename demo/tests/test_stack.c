d30a49f7ae2c35c0b682dbaf9987230d2c328cf1
Ardalan Samimi <ardalan.samimi@gmail.com>
2017-12-29 00:29:52 +0100

    Fixed tests for Stack

--- demo/tests/test_stack.c
+++ demo/tests/test_stack.c
@@ -4,34 +4,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "../stack.h"
-
-// -------------------------------
-// Higher order functions
-// -------------------------------
-
-void free_fun(void *element) {
-  CU_PASS("Free");
-}
+#include "../utils.h"
+#include "../../src/refmem.h"
 
 // -------------------------------
 // The tests
 // -------------------------------
 
-
 void test_stack_new() {
   stack_s *stack = stack_new(NULL);
   CU_ASSERT_EQUAL(stack_size(stack), 0);
 
-  stack_delete(stack);
+  deallocate(stack);
 }
 
 void test_stack_size() {
   stack_s *stack = stack_new(NULL);
+  retain(stack);
   CU_ASSERT_EQUAL(stack_size(stack), 0);
 
-  char *elem1 = "Foo";  
-  char *elem2 = "Bar";
-  char *elem3 = "Baz";
+  char *elem1 = string_duplicate("Foo");
+  char *elem2 = string_duplicate("Bar");
+  char *elem3 = string_duplicate("Baz");
 
   stack_push(stack, elem1);
   CU_ASSERT_EQUAL(stack_size(stack), 1);
@@ -51,24 +45,25 @@ void test_stack_size() {
 
   stack_push(stack, elem3);
   CU_ASSERT_EQUAL(stack_size(stack), 1);
-  
-  stack_delete(stack);
+
+  release(stack);
 }
 
 void test_stack_push() {
   stack_s *stack = stack_new(NULL);
+  retain(stack);
 
-  char *elem1 = "Foo";  
-  char *elem2 = "Bar";
-  char *elem3 = "Baz";
-  char *elem4 = "Haz";
-  
+  char *elem1 = string_duplicate("Foo");
+  char *elem2 = string_duplicate("Bar");
+  char *elem3 = string_duplicate("Baz");
+  char *elem4 = string_duplicate("Haz");
+
   stack_push(stack, elem1);
   CU_ASSERT_STRING_EQUAL(stack_top(stack), elem1);
   stack_push(stack, elem2);
-  CU_ASSERT_STRING_EQUAL(stack_top(stack), elem2); 
+  CU_ASSERT_STRING_EQUAL(stack_top(stack), elem2);
   stack_push(stack, elem3);
-  CU_ASSERT_STRING_EQUAL(stack_top(stack), elem3); 
+  CU_ASSERT_STRING_EQUAL(stack_top(stack), elem3);
 
   stack_pop(stack);
   CU_ASSERT_STRING_EQUAL(stack_top(stack), elem2);
@@ -79,17 +74,19 @@ void test_stack_push() {
 
   stack_push(stack, elem4);
   CU_ASSERT_STRING_EQUAL(stack_top(stack), elem4);
-  
-  stack_delete(stack);
+
+  release(stack);
 }
 
 void test_stack_pop() {
   stack_s *stack = stack_new(NULL);
+  retain(stack);
+
   CU_ASSERT_PTR_NULL(stack_pop(stack));
-  
-  char *elem1 = "Foo";  
-  char *elem2 = "Bar";
-  char *elem3 = "Baz";
+
+  char *elem1 = string_duplicate("Foo");
+  char *elem2 = string_duplicate("Bar");
+  char *elem3 = string_duplicate("Baz");
 
   stack_push(stack, elem1);
   stack_push(stack, elem2);
@@ -98,20 +95,22 @@ void test_stack_pop() {
   CU_ASSERT_EQUAL(stack_size(stack), 1);
   CU_ASSERT_STRING_EQUAL(stack_pop(stack), elem1);
   CU_ASSERT_EQUAL(stack_size(stack), 0);
-  
+
   stack_push(stack, elem3);
   CU_ASSERT_STRING_EQUAL(stack_pop(stack), elem3);
-  
-  stack_delete(stack);
+
+  release(stack);
 }
 
 void test_stack_top() {
   stack_s *stack = stack_new(NULL);
+  retain(stack);
+
   CU_ASSERT_PTR_NULL(stack_top(stack));
-  
-  char *elem1 = "Foo";  
-  char *elem2 = "Bar";
-  char *elem3 = "Baz";
+
+  char *elem1 = string_duplicate("Foo");
+  char *elem2 = string_duplicate("Bar");
+  char *elem3 = string_duplicate("Baz");
 
   stack_push(stack, elem1);
   CU_ASSERT_STRING_EQUAL(stack_top(stack), elem1);
@@ -126,17 +125,7 @@ void test_stack_top() {
   CU_ASSERT_STRING_EQUAL(stack_top(stack), elem2);
   CU_ASSERT_EQUAL(stack_size(stack), 2);
 
-  stack_delete(stack);
-}
-
-void test_stack_delete() {
-  stack_s *stack = stack_new(free_fun);
-
-  char *elem1 = "Foo";
-
-  stack_push(stack, elem1);
-
-  stack_delete(stack);
+  release(stack);
 }
 
 int main(int argc, char *argv[]) {
@@ -149,11 +138,10 @@ int main(int argc, char *argv[]) {
   CU_add_test(stack, "Push", test_stack_push);
   CU_add_test(stack, "Pop", test_stack_pop);
   CU_add_test(stack, "Top", test_stack_top);
-  CU_add_test(stack, "Delete", test_stack_delete);
 
   CU_basic_run_tests();
 
   CU_cleanup_registry();
-  
+
   return CU_get_error();
 }
